3f13aa552237ab5b4a13bab17de08b776c349fa7
jabeangelo6-sudo <jabeangelo6@gmail.com>
2026-06-17 16:41:31 +0200

    fix: lazy db init + fix db count query

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,7 +2,21 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql, { schema });
+type DB = ReturnType<typeof drizzle<typeof schema>>;
+let _db: DB | undefined;
+
+function getDb(): DB {
+  if (!_db) {
+    const sql = neon(process.env.DATABASE_URL!);
+    _db = drizzle(sql, { schema });
+  }
+  return _db;
+}
+
+export const db: DB = new Proxy({} as DB, {
+  get(_, key) {
+    return (getDb() as any)[key as string];
+  },
+});
 
 export * from "./schema";
